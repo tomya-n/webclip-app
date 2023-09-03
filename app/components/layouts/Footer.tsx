3c0e5168e70208e162d781d98950602a07d69118
tom <tomcan1126@gmail.com>
2023-09-04 01:17:47 +0900

    change style変更

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <>
-      <footer className="bg-sky-200 text-center py-1">
+      <footer className="bg-blue-600 text-center py-1">
         <small className="text-slate-400">&copy; 2023 tom-n</small>
       </footer>
     </>
