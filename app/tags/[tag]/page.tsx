021f58ca442264c50b4bb787d0f7d86e9627cc11
tom <tomcan1126@gmail.com>
2023-12-02 20:42:27 +0900

    タグごとのリスト表示に対応

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,7 +1,26 @@
-export default function Home({ params }: { params: { tag: string } }) {
+import ClipItem from "../../components/ClipItem";
+import { ClipData } from "../../@types/index.d";
+
+export default async function Home({ params }: { params: { tag: string } }) {
+  const resClipData = await fetch(`http://localhost:5555/clipData`, {
+    cache: "no-cache",
+  });
+  const clipData = await resClipData.json();
+  const filterClipData: ClipData[] = [];
+  clipData.filter((clip) => (clip.tags.includes(params.tag) ? filterClipData.push(clip) : null));
+  // const res = await fetch(`http://localhost:5555/tags/${params.tag}`, {
+  //   cache: "no-cache",
+  // });
+
+  console.log(clipData);
+  console.log(filterClipData);
+
   return (
     <>
       <div>{params.tag}の一覧</div>
+      <ul className="basis-11/12">
+        <ClipItem clipData={filterClipData} />
+      </ul>
     </>
   );
 }
